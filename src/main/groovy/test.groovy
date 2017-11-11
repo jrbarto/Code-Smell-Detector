591498e6cd95b9be06d0c751c60bd48b3ab21f0a
Jeffrey Barto <jrbarto@users.noreply.github.com>
2017-11-11 18:22:50 -0500

    Update test.groovy

--- src/main/groovy/test.groovy
+++ src/main/groovy/test.groovy
@@ -1,4 +1,4 @@
 import com.codeparser.Driver
 
+// Utilize our Code-Parser API to print each bracket starting from the global scope
 Driver.printGlobal(this.args[0])
-println("This is only a tedddddddst groovy script...")
