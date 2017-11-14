1e13613edf4293161586f681899a13a18e0cc3e1
Brian Green <brian@Brians-MacBook-Pro-2.local>
2017-11-14 13:02:00 -0500

    Added MethodTooManyArgs.groovy

--- src/main/groovy/MethodTooManyArgs.groovy
+++ src/main/groovy/MethodTooManyArgs.groovy
@@ -3,9 +3,9 @@ import com.codeparser.Driver
 def filename = args[0]
 int maxArgs = 3
 
-// The Bracket class should be created only once per filename or it be
-// exponentially slower because we are reading from the file unnecessary times 
-// Therefore, the caller of this method should have the object and lines[].
+// A Bracket object should be created only once per filename or it will be
+// exponentially slower because we are reading from the disk unnecessary times. 
+// Therefore, the caller of this method should pass the object and lines[].
 // Note: This method is a okay for sandbox but not for production.
 
 Driver.mTooManyArgs(filename, maxArgs)
