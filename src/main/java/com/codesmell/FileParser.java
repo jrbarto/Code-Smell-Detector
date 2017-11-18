e1eb104eeffbe5363767b8a050018cbb0647c03d
Jeff Barto <jrbarto@us.ibm.com>
2017-11-18 14:57:11 -0500

    Fixed to run correct groovy.bat file for Windows.

--- src/main/java/com/codesmell/FileParser.java
+++ src/main/java/com/codesmell/FileParser.java
@@ -6,8 +6,12 @@ public class FileParser {
 	File groovyDir;
 	File groovyFile;
 	File sourceFile;
+	boolean isWindows;
 
 	public FileParser(String groovyArg, String sourceArg) {
+		String os = System.getProperty("os.name");
+		isWindows = os.toLowerCase().contains("windows");
+
 		groovyFile = new File(groovyArg);
 		sourceFile = new File(sourceArg);
 
@@ -23,8 +27,16 @@ public class FileParser {
 
 	public void runGroovyCommand() {
 		ProcessHelper procHelper = new ProcessHelper(groovyDir);
+		String groovyCommand; // must be on system path
+
+		if (isWindows) {
+			groovyCommand = "groovy.bat";
+		}
+		else {
+			groovyCommand = "groovy";
+		}
+
 		String message = "Running groovy file " + groovyFile.getName() + " on source file " + sourceFile.getName();
-		String groovyCommand = "groovy"; // must be on system path, but will change in future updates
 		String classpath = System.getProperty("java.class.path");
 		String[] args = {groovyCommand, "-cp", classpath, groovyFile.getAbsolutePath(), sourceFile.getAbsolutePath()};
 
